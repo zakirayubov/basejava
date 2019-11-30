10ff4a332a5a3991dcdbdd99ce632a8d8665d7df
Magomedzakir Ayubov <zakirayubov@mail.ru>
2019-11-30 20:25:19 +0300

    3_2_implements interface

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,5 +1,6 @@
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.ArrayStorage;
+import ru.javawebinar.basejava.storage.Storage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import java.io.InputStreamReader;
  * (just run, no need to understand)
  */
 public class MainArray {
-    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private final static Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
